24088ef2921986b2296e2536618d9fab3e37eb6c
xiehaopei <15907678645@163.com>
2020-06-02 12:36:44 +0800

    数据统计

---
+++ src/components/report/Report.vue
@@ -0,0 +1,88 @@
+<!-- 组件说明 -->
+<template>
+  <div class=''>
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片 -->
+    <el-card>
+      <div id="main"
+           style="width: 800px;height:400px;"></div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+//导入echarts
+import echarts from 'echarts'
+import _ from 'lodash'
+
+//import x from ''
+export default {
+  components: {
+
+  },
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+  async mounted () {
+    // 基于准备好的dom，初始化echarts实例
+    let myChart = echarts.init(document.getElementById('main'));
+
+    //获取报表数据
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) return this.$message.error('获取报表数据失败！')
+
+    //合并对象
+    const result = _.merge(res.data, this.options)
+    console.log(result)
+
+    // 绘制图表
+    myChart.setOption(result)
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
